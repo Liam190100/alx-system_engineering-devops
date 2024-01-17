dd7084a509f343214244f0868b99d6077f330cf6
Liam190100 <jnnjuguna8@gmail.com>
2024-01-17 12:00:19 -0800

    puppet

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixing `phpp` in file to `php` in file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
